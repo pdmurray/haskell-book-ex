636625b7ee96828f4cefaf712361f7e002dcd430
Patrick Murray <patrmurr@cisco.com>
2017-05-10 15:12:10 -0700

    complete exercises 9.8

---
+++ src/ch9/Exercises9.8.hs
@@ -0,0 +1,26 @@
+
+-- Bottom Madness
+
+{-
+1.  bottom
+2.  returns
+3.  bottom
+4.  returns
+5.  bottom
+6.  returns
+7.  bottom
+8.  returns
+9.  returns
+10. bottom
+
+-- Is it in normal form?
+
+1. 1
+2. 2
+3. 3
+4. 3
+5. 3
+6. 3
+7. 2
+
+-}
