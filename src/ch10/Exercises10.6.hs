c0d26ee69f5452c5210469b72b6dad626f312d21
Patrick Murray <patrmurr@cisco.com>
2017-05-24 22:30:27 -0700

    Finished section 10.6

--- src/ch10/Exercises10.6.hs
+++ src/ch10/Exercises10.6.hs
@@ -18,5 +18,38 @@ theDatabase =
     , DbDate (UTCTime
               (fromGregorian 1921 5 1)
               (secondsToDiffTime 34123))
+    , DbNumber 8005
   ]
-  
+
+-- 1. 
+filterDbDate :: [DatabaseItem] -> [UTCTime]
+filterDbDate = foldr f []
+  where f (DbDate x) xs = x : xs
+        f _ xs = xs
+
+-- 2.
+filterDbNumber :: [DatabaseItem] -> [Integer]
+filterDbNumber = foldr f []
+  where f (DbNumber x) xs = x : xs
+        f _ xs = xs
+
+-- 3.
+mostRecent :: [DatabaseItem] -> UTCTime
+mostRecent = maximum . filterDbDate
+
+-- 4. 
+sumDb :: [DatabaseItem] -> Integer
+sumDb = sum . filterDbNumber
+
+-- 5.
+avgDb :: [DatabaseItem] -> Double
+{-
+avgDb x = dbNumberSum / dbNumberLength
+  where dbNumberSum = fromIntegral $ sumDb x
+        dbNumberLength = fromIntegral (length $ filterDbNumber x)
+-}
+avgDb = average . filterDbNumber
+
+average :: (Integral a) => [a] -> Double
+average [] = 0.0
+average xs = fromIntegral (sum xs) / fromIntegral (length xs)
