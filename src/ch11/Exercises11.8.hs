fa850ef895d6e41e1e856b167e6796e62e0e3c19
Patrick Murray <patrmurr@cisco.com>
2017-06-08 21:33:45 -0700

    completed section 11.8

---
+++ src/ch11/Exercises11.8.hs
@@ -0,0 +1,18 @@
+module Exercises118 where
+
+-- Exercises: Cardinality
+
+{-
+1.  Nullary
+2.  3
+3.  65536
+4.
+Prelude> minBound :: Int
+-9223372036854775808
+Prelude> maxBound :: Int
+9223372036854775807
+
+Integer does not have an instance of bounded, so appears to be unbounded.
+
+5.  the '8' in Int8 refers to 8 bits => 2^8 = 256.  Likewise with Int16, etc.
+-}
