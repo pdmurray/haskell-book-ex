f6da2bbca6e796fd23f0f16a52986d617aa9fad5
Patrick Murray <patrmurr@cisco.com>
2017-06-12 22:14:14 -0700

    forgot to save before adding problem 2 in 11.10

--- src/ch11/Exercises11.10.hs
+++ src/ch11/Exercises11.10.hs
@@ -3,7 +3,10 @@ module Exercises1110 where
 -- Exercises: Pity the Bool
 
 -- 1.  Cardinality: 4.  Bool is 2 and sum type is addition.
--- 2.  
+-- 2.  The cardinality is 2 + 256 = 258.
+--     If you try to create a numeric literal less than -128
+--     or greater than 127, you get an error:
+--     "Literal 130 is out of the Int8 range -128..127"
 
 -- needed to have Int8 in scope
 import Data.Int
