953e02a8e4decd9f3717a9f04b59a2d177bf6468
Patrick Murray <patrmurr@cisco.com>
2017-06-15 22:19:42 -0700

    finished multiple choice of 11.18 (chapter exercises)

---
+++ src/ch11/Exercises11.18.hs
@@ -0,0 +1,17 @@
+module Exercises1118 where
+
+-- Chapter Exercises
+
+
+-- Multiple Choice
+data Weekday =
+      Monday
+    | Tuesday
+    | Wednesday
+    | Thursday
+    | Friday
+
+-- 1.  a
+-- 2.  c
+-- 3.  b
+-- 4.  c
