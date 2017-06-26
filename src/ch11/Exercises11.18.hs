71e287d4d387b00e9e087abd110e56aff1819113
Patrick Murray <patrmurr@cisco.com>
2017-06-26 16:15:11 -0700

    working on ch18 exercises

--- src/ch11/Exercises11.18.hs
+++ src/ch11/Exercises11.18.hs
@@ -21,3 +21,39 @@ data Weekday =
 
 -- Ciphers
 
+
+
+-- As-patterns
+
+-- 1. 
+isSubsequenceOf :: (Eq a) => [a] -> [a] -> Bool
+isSubsequenceOf [] [] = True
+isSubsequenceOf [] _  = True
+isSubsequenceOf _ []  = False
+isSubsequenceOf xz@(x:xs) (y:ys)
+  | x == y = isSubsequenceOf xs ys
+  | otherwise = isSubsequenceOf xz ys
+
+
+--2.
+{-
+Prelude> capitalizeWords "hello world"
+     [("hello", "Hello"), ("world", "World")]
+-}
+splitter :: (Eq a) => a -> [a] -> [[a]]
+splitter _ [] = []
+splitter sep input = beg : end
+    where
+        notSep = \x -> x /= sep
+        isSep = \x -> x == sep
+        beg = takeWhile notSep input
+        end = splitter sep . dropWhile isSep . dropWhile notSep $ input
+
+splitWords :: String -> [String]
+splitWords = splitter ' '
+
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords x = map capitalizeWord $ splitWords x
+
+capitalizeWord :: String -> (String, String)
+capitalizeWord xz@(x:xs) = (xz, toUpper x : xs)
