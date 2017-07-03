66fa3bb7da25f2d5b8c2d5dc8424e4d421f3dbf4
Patrick Murray <patrmurr@cisco.com>
2017-07-02 21:30:02 -0700

    started on ch.12 exercises

--- src/ch12/Exercises12.5.hs
+++ src/ch12/Exercises12.5.hs
@@ -1,2 +1,10 @@
 module Exercises125 where
 
+-- Determine the Kinds
+
+-- 1.  id :: a -> a    a has kind *
+-- 2.  r :: a -> f a   a has kind *, f a has kind * -> *
+
+-- String Processing
+
+-- 1. 
