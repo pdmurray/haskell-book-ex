232f3395239d0516225277c85e8f1f2742388135
Patrick Murray <patrmurr@cisco.com>
2017-08-02 21:42:47 -0700

    figured out how to map over Maybe and just return the 'success' values, used in countVowels function

--- src/ch12/Exercises12.5.hs
+++ src/ch12/Exercises12.5.hs
@@ -1,5 +1,7 @@
 module Exercises125 where
 
+import Data.Maybe
+
 -- Determine the Kinds
 
 -- 1.  id :: a -> a    a has kind *
@@ -20,10 +22,30 @@ notThe :: String -> Maybe String
 notThe "the" = Nothing
 notThe x = Just x
 
+theToA :: Maybe String -> String
+theToA Nothing = "a"
+theToA (Just x) = x
+
 -- >>> replaceThe "the cow loves us" -- "a cow loves us"
 replaceThe :: String -> String
 replaceThe x = unwords $ map (theToA . notThe) $ words x
 
-theToA :: Maybe String -> String
-theToA Nothing = "a"
-theToA (Just x) = x
+
+-- 2.
+vowelInitial :: String -> Maybe String
+vowelInitial x
+    | head x `elem` "aeiou" = Nothing
+    | otherwise = Just x
+
+countTheBeforeVowel :: String -> Integer
+countTheBeforeVowel = undefined
+
+
+-- 3.
+isVowel :: Char -> Maybe Char
+isVowel x
+    | x `elem` "aeiou" = Just x
+    | otherwise = Nothing
+
+countVowels :: String -> Integer 
+countVowels = toInteger . length . mapMaybe isVowel
